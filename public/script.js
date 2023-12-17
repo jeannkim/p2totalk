64f8d5695e6f05c9095cc550438dcc215358cfb9
jeannie <jlk004@ucsd.edu>
2023-12-17 02:29:50 -0800

    Working button pairs delimiter logic

--- public/script.js
+++ public/script.js
@@ -1,5 +1,3 @@
-// just working out the logic
-
 /**
  * Keypresses to buttons
  *
@@ -46,38 +44,73 @@ const keyMaps = {
    'v': 'R2'
 }
 
-document.addEventListener('keydown', (event) => {
-   const output = document.getElementById('keypress');
-   const allpresses = document.getElementById('allpresses');
-   const button = keyMaps[event.key];
-   const gotButton = button ? button : 'INVALID';
-   // print the button press
-   output.textContent = gotButton;
-   allpresses.textContent = allpresses.textContent + ' ' + gotButton;
-});
-
 
 /**
+ * Split by button press combinations
+ * First inputs: everything but L-R buttons
+ * Second inputs: L1, L2, R1, R2, or nothing
+ * Ex. (Triangle L1) (Square) (Circle R2)
  * 
- */
- // initialize lists
- const faceButtons = ["TRIANGLE", "SQUARE", "X", "CIRCLE"];
- const dPad = ["UP", "LEFT", "DOWN", "RIGHT"];
- const start = ["START"];
-
- const firstInput = faceButtons + dPad + start;
- 
- const shoulderButtons = ["L2", "L1", "R1", "R2"];
- 
- const secondInput = shoulderButtons + ""; // no input = empty string???
-
-
-/**
- * First input is paired with (or not with) the second input
- * Second input can be nonexistent
- * Produces a phoneme.
+ * currButtonPair: phoneme button input (length 1 or 2)
+ * allButtonPairs: list of all phoneme combinations
+ * secondInputs: const set of all second inputs
+ * invalidPairs: pairs there are no phonemes for
  * 
 */
+var currButtonPair = [];
+var allButtonPairs = [];
+const secondInputs = new Set(['L1', 'L2', 'R1', 'R2']);
+// in strings and not arrays because JS compares arrays by ref
+const invalidPairs = new Set([
+   'Square,R1', 
+   'Circle,L1', 
+   'Circle', 
+   'Left,R1',
+   'Right,L2'
+]);
+
+document.addEventListener('keydown', (event) => {
+   let output = document.getElementById('keypress');
+   let allPresses = document.getElementById('allpresses');
+   let allPairs = document.getElementById('allpairs');
+   let button = keyMaps[event.key];
+   
+   // get button press, output button presses
+   let currButton = button ? button : 'INVALID';
+   output.textContent = currButton;
+   allPresses.textContent = allPresses.textContent + ' ' + currButton;
+
+   // second input: just add button as second value
+   if (currButton != 'INVALID'){
+      if (secondInputs.has(currButton)){
+         // can only add as second input
+         if (currButtonPair.length == 1){
+            currButtonPair.push(currButton);
+         }
+      }
+      else { // first input
+         // if there is anything in the curr pair, flush it
+         if (currButtonPair.length > 0){
+            // check if the curr pair is valid
+            console.log(currButtonPair.toString());
+            console.log(!invalidPairs.has(currButtonPair.toString()));
+            if (!invalidPairs.has(currButtonPair.toString())){
+               // save previous pair
+               allButtonPairs.push(currButtonPair);
+               // *just for printing
+               currButtonPair.forEach(function(button) {
+                  allPairs.textContent = allPairs.textContent + ' ' + button;
+               });
+               allPairs.textContent = allPairs.textContent + ' / ';
+            }
+            // empty the array
+            currButtonPair = [];
+         }
+         // add to pair
+         currButtonPair.push(currButton); // add curr
+      }
+   }
+});
 
 /**
  * build the table of combinations (translate each combination to corresponding phoneme)
